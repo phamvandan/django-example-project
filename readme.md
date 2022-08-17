cb7da5421ce685a06a0b6023f7e474721825debd
phamvandan <phamvandan.cntt2@gmail.com>
2022-08-17 10:05:23 +0700

    update change

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Installation
+1. https://drf-yasg.readthedocs.io/en/stable/readme.html#usage
+2. 
--- rest_example_project/student/views.py
+++ rest_example_project/student/views.py
@@ -4,8 +4,10 @@ from .serializers import StudientSerializer
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework.renderers import StaticHTMLRenderer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 # Create your views here.
+
 
 class StudentViewSet(viewsets.ModelViewSet):
     """
@@ -20,12 +22,19 @@ class StudentViewSet(viewsets.ModelViewSet):
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly]
+    test_param1 = openapi.Parameter('check_type', openapi.IN_QUERY, description="height or width", type=openapi.TYPE_STRING)
+    test_param2 = openapi.Parameter('id', openapi.IN_QUERY, description="id of student", type=openapi.TYPE_NUMBER)
+    user_response = openapi.Response('response description', StudientSerializer)
 
+    @swagger_auto_schema(method='get', manual_parameters=[test_param1, test_param2], responses={404: 'Not found', 200:'ok', 201:StudientSerializer})
     @action(methods=['GET'], detail=False, url_path='check-body')
     def check_body(self, request):
+        """
+        Check body API
+        """
         check_type = request.query_params.get('check_type')
         id = request.query_params.get('id')
-        
+
         print(check_type, id)
         obj = self.queryset.get(id=id)
         rs = ""
@@ -39,8 +48,8 @@ class StudentViewSet(viewsets.ModelViewSet):
                 rs = "fat"
             else:
                 rs = "thin"
-        return Response({"result":rs})
-    
+        return Response({"result": rs})
+
     # @action(methods=['POST'], detail=False)
     # def echo(self, request):
     #     # deserializer
@@ -54,4 +63,4 @@ class StudentViewSet(viewsets.ModelViewSet):
     #     name = request.query_params.get('name')
     #     print(self.queryset)
     #     objs = Student.objects.filter(name__exact=name)
-    #     return Response(StudientSerializer(objs).data)  
+    #     return Response(StudientSerializer(objs).data)
